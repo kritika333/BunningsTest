73e2bf0d412fd25a1b846db217db615f3a8cfb84
Kritika Gupta <Kritikagupta333@gmail.com>
2021-11-15 00:01:18 +1100

    Updating test cases and config changes

--- src/test/java/stepDefinition/search.java
+++ src/test/java/stepDefinition/search.java
@@ -5,38 +5,40 @@ import PageObjects.UImodule;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
+import java.io.IOException;
+
 public class search extends UImodule {
 
-    HomePage homepage = new HomePage(driverThread);
-
-    @Given("User lands to home page")
-    public void userOnHomePage()
-    {
+    @Given("Search functionality should be tested")
+    public void searchtest(){
         DriverConfiguration();
+
+    }
+
+    @Then("User lands to home page")
+    public void userOnHomePage() throws IOException {
+        HomePage homepage = new HomePage(driverThread);
         homepage.homeinit();
-        throw new io.cucumber.java.PendingException();
     }
 
     @Then("Search Bar should appear")
     public void checkSearchBar()
-    {
+    {    HomePage homepage = new HomePage(driverThread);
         homepage.checkSearchbar();
-        throw new io.cucumber.java.PendingException();
     }
 
     @Then("Base text in search bar should be What can we help you find today?")
     public void baseText()
-    {
+    {    HomePage homepage = new HomePage(driverThread);
         homepage.baseText();
-        throw new io.cucumber.java.PendingException();
     }
 
 
     @Then("User should be able to enter text in search bar")
 
     public void editSearchfield()
-    {
+    {    HomePage homepage = new HomePage(driverThread);
         homepage.editSearchfield();
-        throw new io.cucumber.java.PendingException();
+        QuitDriver(driverThread);
     }
 }
