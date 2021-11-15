d0f8a632509dfb0e5b7b29bee692d42a9225ed75
Kritika Gupta <Kritikagupta333@gmail.com>
2021-11-15 12:51:40 +1100

    Updating test cases and config changes

--- src/test/java/stepDefinition/search.java
+++ src/test/java/stepDefinition/search.java
@@ -1,21 +1,15 @@
 package stepDefinition;
 
-import PageObjects.HomePage;
-import PageObjects.UImodule;
+
+import Core.UImodule;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
 
 public class search extends UImodule {
 
@@ -25,24 +19,32 @@ public class search extends UImodule {
 
     }
 
-    @When("User lands to home page")
-    public void userOnHomePage() throws IOException {
+//    @When("User lands to home page")
+//    public void userOnHomePage() {
+//        driverThread.get("https://www.bunnings.com.au");
+//        Assert.assertTrue(driverThread.findElement(By.xpath("//*[@data-locator='logo-Bunnings']")).isDisplayed());
+//    }
+
+    @When("User is on home page")
+    public void userOnHomePage() {
+        DriverConfiguration();
         System.out.println("inside homeinit");
         driverThread.get("https://www.bunnings.com.au");
+        Assert.assertTrue(driverThread.findElement(By.xpath("//*[@data-locator='logo-Bunnings']")).isDisplayed());
     }
 
-    @Then("Search Bar should appear")
+    @Given("Search Bar should appear")
     public void checkSearchBar()
     {
-        if(driverThread.findElement(By.id("custom-css-outlined-input")).isDisplayed())
+        Assert.assertTrue(driverThread.findElement(By.id("custom-css-outlined-input")).isDisplayed());
         System.out.println("Search bar displayed successfully");
     }
 
     @Then("Base text in search bar should be What can we help you find today?")
     public void baseText()
     {
-        if(driverThread.findElement(By.id("custom-css-outlined-input")).getText().contains("What can we help you find today?"))
-        System.out.println("Base text in Search bar displayed successfully");
+        Assert.assertEquals(driverThread.findElement(By.id("custom-css-outlined-input")).getText(),"What can we help you find today?");
+                System.out.println("Base text in Search bar displayed successfully");
     }
 
 
@@ -51,35 +53,59 @@ public class search extends UImodule {
     public void editSearchfield()
     {
         driverThread.findElement(By.id("custom-css-outlined-input")).sendKeys("stud finder");
-        driverThread.findElement(By.id("crossIcon")).click();
+                driverThread.findElement(By.id("crossIcon")).click();
         if(driverThread.findElement(By.id("custom-css-outlined-input")).getText().contains("stud finder"))
             System.out.println("Field edited successfully");
     }
 
-   @And("^User Enters ([^\"]*) in search bar$")
+    @Then("Popular searches should appear on search bar click")
+    public void flyoutdisplay()
+    {
+        driverThread.findElement(By.id("custom-css-outlined-input")).click();
+        Assert.assertTrue(driverThread.findElement(By.id("flyout")).isDisplayed());
+        QuitDriver(driverThread);
+    }
+
+
+    @And("^User Enters ([^\"]*) in search bar$")
     public void enterProduct(String product)
-   {
-       driverThread.findElement(By.id("custom-css-outlined-input")).sendKeys(product);
-       driverThread.findElement(By.id("crossIcon")).click();
+    {
+        driverThread.findElement(By.id("custom-css-outlined-input")).sendKeys(product);
+        driverThread.findElement(By.id("crossIcon")).click();
 
-   }
+    }
 
-   @Then("Search results should appear")
+    @Then("Search results should appear")
     public void searchResult()
-   {
-          // Assert.assertTrue(driverThread.findElement(By.xpath("//*[@class='searchTerm']")).isDisplayed());
-       WebDriverWait wait=new WebDriverWait(driverThread,30);
-       wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='searchTerm']"))).isDisplayed();
-   }
+    {
+        // Assert.assertTrue(driverThread.findElement(By.xpath("//*[@class='searchTerm']")).isDisplayed());
+        WebDriverWait wait=new WebDriverWait(driverThread,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='searchTerm']"))).isDisplayed();
+    }
 
-   @Then("^Total number of result ([^\"]*) should appear$")
-   public void totalProduct(String total)
-   {
-       driverThread.findElement(By.xpath("//div[@class='totalResults']/p")).getText().contains(total);
-       QuitDriver(driverThread);
-   }
+    @Then("^Total number of result ([^\"]*) should appear$")
+    public void totalProduct(String total)
+    {
+        driverThread.findElement(By.xpath("//div[@class='totalResults']/p")).getText().contains(total);
+        QuitDriver(driverThread);
+    }
+
+    @When("User select item from flyout")
+    public void selectItem()
+    {
+        driverThread.findElement(By.xpath("(//*[@data-code='0177356'])[1]")).click();
+
+    }
+
+    @Then("User should navigate to item page")
+    public void navigatePDP()
+    {
+        Assert.assertTrue(driverThread.findElement(By.xpath("//*[@class='productDescriptionTopWrapper']")).isDisplayed());
+    }
+
+    @And("Add to cart button should displayed")
+    public void addtoCart()
+    {
+        Assert.assertTrue(driverThread.findElement(By.xpath("//*[@data-locator='atcButton']")).isDisplayed());
+    }
 }
-
-
-
-
--- src/test/resources/search.feature
+++ src/test/resources/search.feature
@@ -4,22 +4,51 @@
 Feature: Verify the search feature
 
 
+#  Scenario: Search bar should appear in Home page
+#    Given Search functionality should be tested
+#    When User lands to home page
+#    Then Search Bar should appear
+#    And Base text in search bar should be What can we help you find today?
+#    Then User should be able to enter text in search bar
+#
+#  @kritika
+#  Scenario Outline: Product results should appear on search
+#    Given Search functionality should be tested
+#    When User lands to home page
+#    Then Search Bar should appear
+#    And User Enters <Product> in search bar
+#    Then  Search results should appear
+#    And Total number of result <Totalresults> should appear
+#    Examples:
+#    |  Product              |    Totalresults     |
+#    |  stud finder          |       10            |
+#    |  Egg chair            |        5            |
+
+
+  @kritika
   Scenario: Search bar should appear in Home page
-    Given Search functionality should be tested
-    When User lands to home page
+    Given  User is on home page
     Then Search Bar should appear
     And Base text in search bar should be What can we help you find today?
-    Then User should be able to enter text in search bar
+    Then popular searches should appear on search bar click
 
   @kritika
   Scenario Outline: Product results should appear on search
-    Given Search functionality should be tested
-    When User lands to home page
+    Given  User is on home page
     Then Search Bar should appear
     And User Enters <Product> in search bar
     Then  Search results should appear
     And Total number of result <Totalresults> should appear
     Examples:
-    |  Product              |    Totalresults     |
-    |  stud finder          |       10            |
-    |  Egg chair            |        5            |
+      |  Product              |    Totalresults     |
+      |  stud finder          |       10            |
+      |  Egg chair            |        5            |
+
+  @kritika
+  Scenario: User should be able to select item from flyout
+    Given  User is on home page
+    Then Search Bar should appear
+    Then Popular searches should appear on search bar click
+    When User select item from flyout
+    Then User should navigate to item page
+    And  Add to cart button should displayed
