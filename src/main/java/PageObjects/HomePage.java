b29aac77942645a3c888206fec9c18a062dc0721
Kritika Gupta <Kritikagupta333@gmail.com>
2021-11-15 11:21:21 +1100

    Updating test cases and config changes

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -1,5 +1,6 @@
 package PageObjects;
 
+import com.google.common.graph.AbstractNetwork;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -37,6 +38,18 @@ public class HomePage extends UImodule {
         driverThread.findElement(By.id("crossIcon")).click();
         if(driverThread.findElement(By.id("custom-css-outlined-input")).getText().contains("stud finder"))
             System.out.println("Field edited successfully");
+    }
+
+
+    public void enterProduct(String product)
+    {
+        driverThread.findElement(By.id("custom-css-outlined-input")).sendKeys(product);
+        driverThread.findElement(By.id("crossIcon")).click();
+    }
+
+    public void searchResult()
+    {
+        driverThread.findElement(By.className("searchTermContainer"));
     }
 
 }
--- src/test/java/stepDefinition/search.java
+++ src/test/java/stepDefinition/search.java
@@ -2,10 +2,20 @@ package stepDefinition;
 
 import PageObjects.HomePage;
 import PageObjects.UImodule;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 public class search extends UImodule {
 
@@ -15,30 +25,61 @@ public class search extends UImodule {
 
     }
 
-    @Then("User lands to home page")
+    @When("User lands to home page")
     public void userOnHomePage() throws IOException {
-        HomePage homepage = new HomePage(driverThread);
-        homepage.homeinit();
+        System.out.println("inside homeinit");
+        driverThread.get("https://www.bunnings.com.au");
     }
 
     @Then("Search Bar should appear")
     public void checkSearchBar()
-    {    HomePage homepage = new HomePage(driverThread);
-        homepage.checkSearchbar();
+    {
+        if(driverThread.findElement(By.id("custom-css-outlined-input")).isDisplayed())
+        System.out.println("Search bar displayed successfully");
     }
 
     @Then("Base text in search bar should be What can we help you find today?")
     public void baseText()
-    {    HomePage homepage = new HomePage(driverThread);
-        homepage.baseText();
+    {
+        if(driverThread.findElement(By.id("custom-css-outlined-input")).getText().contains("What can we help you find today?"))
+        System.out.println("Base text in Search bar displayed successfully");
     }
 
 
     @Then("User should be able to enter text in search bar")
 
     public void editSearchfield()
-    {    HomePage homepage = new HomePage(driverThread);
-        homepage.editSearchfield();
-        QuitDriver(driverThread);
+    {
+        driverThread.findElement(By.id("custom-css-outlined-input")).sendKeys("stud finder");
+        driverThread.findElement(By.id("crossIcon")).click();
+        if(driverThread.findElement(By.id("custom-css-outlined-input")).getText().contains("stud finder"))
+            System.out.println("Field edited successfully");
     }
+
+   @And("^User Enters ([^\"]*) in search bar$")
+    public void enterProduct(String product)
+   {
+       driverThread.findElement(By.id("custom-css-outlined-input")).sendKeys(product);
+       driverThread.findElement(By.id("crossIcon")).click();
+
+   }
+
+   @Then("Search results should appear")
+    public void searchResult()
+   {
+          // Assert.assertTrue(driverThread.findElement(By.xpath("//*[@class='searchTerm']")).isDisplayed());
+       WebDriverWait wait=new WebDriverWait(driverThread,30);
+       wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='searchTerm']"))).isDisplayed();
+   }
+
+   @Then("^Total number of result ([^\"]*) should appear$")
+   public void totalProduct(String total)
+   {
+       driverThread.findElement(By.xpath("//div[@class='totalResults']/p")).getText().contains(total);
+       QuitDriver(driverThread);
+   }
 }
+
+
+
+
